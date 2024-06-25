fccab3a366e11132a8bd9a7fc800be99bc886a5c
eugene <medov-foto@mail.ru>
2024-06-25 14:56:50 +0300

    изменил отображение профита DWMY зависит от updated_at (при изменении старого ордера, попадает в статистику на дату изменения)

--- app/Repositories/StatisticRepository.php
+++ app/Repositories/StatisticRepository.php
@@ -13,15 +13,15 @@ class StatisticRepository
         return DB::select("
             SELECT
                 sum(orders.total_amount)
-                    FILTER ( WHERE date_trunc('day', created_at) = date_trunc('day', current_date) AND status = 'done')
+                    FILTER ( WHERE date_trunc('day', updated_at) = date_trunc('day', current_date) AND status = 'done')
                     as total_day,
                 sum(orders.total_amount)
-                    FILTER ( WHERE date_trunc('week', created_at) = date_trunc('week', current_date) AND status = 'done')
+                    FILTER ( WHERE date_trunc('week', updated_at) = date_trunc('week', current_date) AND status = 'done')
                     as total_week,
                 sum(orders.total_amount)
-                    FILTER ( WHERE date_trunc('month', created_at) = date_trunc('month', current_date) AND status = 'done')
+                    FILTER ( WHERE date_trunc('month', updated_at) = date_trunc('month', current_date) AND status = 'done')
                     as total_month,
-                sum(orders.total_amount) FILTER ( WHERE date_trunc('year', created_at) = date_trunc('year', current_date) AND status = 'done')
+                sum(orders.total_amount) FILTER ( WHERE date_trunc('year', updated_at) = date_trunc('year', current_date) AND status = 'done')
                     as total_year
             FROM orders
             WHERE
