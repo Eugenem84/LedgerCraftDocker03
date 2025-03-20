42ac009924e99ca1cd4ab3fe02d3c2da0ac79e89
Eugenem84 <medov-foto@mail.ru>
2025-03-20 08:40:37 +0300

    реализовал проверку новой версии приложения quasar spa

--- app/Http/Controllers/AppVersionController.php
+++ app/Http/Controllers/AppVersionController.php
@@ -8,17 +8,21 @@ class AppVersionController extends Controller
 {
     public function checkQuasarAndroidVersion()
     {
-        $files = glob(storage_path('app/app-debug-*.apk'));
+        $files = glob(storage_path('app/public/*.apk'));
         if (empty($files)) {
             return response()->json(['error' => 'дистрибутив не найден'],404);
         }
 
         $latestApk = basename($files[0]);
 
-        preg_match('/app-release-([\d.]+)\.apk', $latestApk, $matches );
+        preg_match('/([\d.]+)(?:-[\w]+)?\.apk/', $latestApk, $matches);
+
+        if (empty($matches[1])) {
+            return response()->json(['error' => 'не удалось извлечь версию из файла'], 400);
+        }
 
         return response()->json([
-            'version' => $matches[1] ?? '1.0.0',
+            'version' => $matches[1],
             'apk_name' => $latestApk
         ]);
     }
