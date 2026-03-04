8df515b200064f159035b0c8a924dc488cd79953
Eugene <medov-foto@mail.ru>
2026-03-04 22:52:54 +0300

    добавлена миграция и 9001 порт во корс настройки

---
+++ database/migrations/2026_03_04_162502_add_timestamps_to_order_service_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('order_service', function (Blueprint $table) {
+            // Старые записи - NULL, новые - автоматически
+            $table->timestamp('created_at')->nullable()->useCurrent();
+            $table->timestamp('updated_at')->nullable()->useCurrent();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('order_service', function (Blueprint $table) {
+            $table->dropColumn(['created_at', 'updated_at']);
+            $table->dropSoftDeletes();
+        });
+    }
+};
